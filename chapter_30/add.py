3d1116e1f8942fa14444f4cfd2c45d7375951784
Marc Altmann <marcaltmann@posteo.de>
2024-01-26 15:41:17 +0100

    Add radd, iadd examples

--- chapter_30/add.py
+++ chapter_30/add.py
@@ -1,12 +1,69 @@
-class Number:
-    def __init__(self, value):
-        self.value = value
+class Commuter1:
+    def __init__(self, val):
+        self.val = val
 
     def __add__(self, other):
-        return Number(self.value + other.value)
+        print('add', self.val, other)
+        return self.val + other
+
+    def __radd__(self, other):
+        print('radd', self.val, other)
+        return other + self.val
+
+
+class Commuter2:
+    def __init__(self, val):
+        self.val = val
+
+    def __add__(self, other):
+        print('add', self.val, other)
+        return self.val + other
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+
+class Commuter3:
+    def __init__(self, val):
+        self.val = val
+
+    def __add__(self, other):
+        print('add', self.val, other)
+        return self.val + other
+
+    def __radd__(self, other):
+        return self + other
+
+
+class Commuter4:
+    def __init__(self, val):
+        self.val = val
+
+    def __add__(self, other):
+        print('add', self.val, other)
+        return self.val + other
+
+    __radd__ = __add__
+
+
+class Commuter5:
+    def __init__(self, val):
+        self.val = val
+
+    def __add__(self, other):
+        print('add', self.val, other)
+        if isinstance(other, Commuter5):
+            other = other.val
+        return Commuter5(self.val + other)
+
+    def __radd__(self, other):
+        print('radd', self.val, other)
+        return Commuter5(other + self.val)
+
+    def __iadd__(self, other):
+        print('iadd', self.val, other)
+        self.val += other
+        return self
 
     def __repr__(self):
-        return f'Number({self.value})'
-
-    def __str__(self):
-        return f'Number with value of {self.value}'
+        return '<Commuter5: %s>' % self.val
